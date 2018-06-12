e674413fbef7093c0e451c30cf8de0d8484bfebb
Brian.Chu <chuchengpeng0119@163.com>
2018-06-12 16:06:18 +0800

    Initial Commit

---
+++ ScaleSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScaleSlider
+//
+//  Created by 储诚鹏 on 2018/6/12.
+//  Copyright © 2018年 储诚鹏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
